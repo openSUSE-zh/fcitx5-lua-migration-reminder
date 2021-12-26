5b50ce162d73b7f7f1243bac82c5ac7793d921f9
marguerite <i@marguerite.su>
2021-12-26 13:39:39 +0800

    finish

--- addon.lua
+++ addon.lua
@@ -3,8 +3,36 @@ local fcitx = require("fcitx")
 fcitx.watchEvent(fcitx.EventType.KeyEvent, "migrate4to5")
 
 function migrate4to5(sym, state, release)
-  if state == fcitx.KeyState.Ctrl and sym == 32 and not release then
-    io.popen("lua ./reminder.lua")
+  local home = os.getenv("HOME")
+  local path = debug.getinfo(1).source:match("@?(.*/)")
+  local reminder = "lua " .. path .. "reminder.lua"
+  local f = io.open(home .. "/.config/fcitx", "r")
+  if f == nil then
+    f.close()
+    return false
+  end
+  f = io.open(home .. "/.config/fcitx/.migration-complete", "r")
+  if f ~= nil then
+    f.close()
+    return false
+  end
+
+  f = io.open(home .. "/.config/fcitx5/.timestamp", "r")
+  if f ~= nil then
+    io.close(f)
+    local cmd = "stat -c %Y " .. home .. "/.config/fcitx5/.timestamp"
+    local timestamp = io.popen(cmd)
+    local last_modified = timestamp:read()
+    local day = math.floor(os.difftime(os.time(), last_modified) / (24*60*60))
+    if day > 0 then
+      if state == fcitx.KeyState.Ctrl and sym == 32 and not release then
+        io.popen(reminder)
+      end
+    end
+  else
+    if state == fcitx.KeyState.Ctrl and sym == 32 and not release then
+      io.popen(reminder)
+    end
   end
   return false
 end
--- reminder.lua
+++ reminder.lua
@@ -1,22 +1,52 @@
 local lgi = require("lgi")
 local Gtk = lgi.Gtk
+local GdkPixbuf = lgi.GdkPixbuf
+
+local locales = {}
+locales.en_US = {
+  window_title = "Fcitx4 configuration data migration reminder",
+  title = "Migration to Fcitx5",
+  text = "\nOur system detects that you have fcitx4 installed before,\nplease click 'ok' to migrate your configuration and data."
+}
+locales.zh_CN = {
+  window_title = "Fcitx4 配置数据迁移提醒",
+  title = "迁移到 Fcitx5",
+  text = "\n系统检测到您之前安装过 Fcitx4，请点击 ok 运行迁移向导完成配置的迁移。"
+}
+
+local lang = os.getenv("LANG"):match("([%a_]+)")
+if locales[lang] == nil then
+  lang = "en_US"
+end
+local window_title = locales[lang]["window_title"]
+local title = locales[lang]["title"]
+local text = locales[lang]["text"]
 
 local buffer = Gtk.TextBuffer {}
+local pixbuf = GdkPixbuf.Pixbuf.new_from_file("/usr/share/icons/hicolor/scalable/apps/fcitx.svg")
 local iter = buffer:get_iter_at_offset(0)
-buffer:insert(iter, "Migration to Fcitx5\nOur system detects that you have fcitx4 installed before,\nplease run `fcitx5-migrator` to migrate your configuration and data.", -1)
+buffer:insert(iter, title, -1)
+local offset = iter:get_offset()
+iter = buffer:get_iter_at_offset(offset)
+buffer:insert_pixbuf(iter, pixbuf:scale_simple(32, 32, "BILINEAR"))
+offset = iter:get_offset() 
+iter = buffer:get_iter_at_offset(offset)
+buffer:insert(iter, text, -1)
 
 local window = Gtk.Window {
-      title = "Fcitx4 to Fcitx5 Migration Reminder",
+      title = window_title,
       border_width = 10,
       default_width = 200,
       default_height = 100,
       window_position = 'CENTER',
       resizable = false,
+      icon = pixbuf:scale_simple(32, 32, "BILINEAR"),
       Gtk.Box {
         orientation = 'VERTICAL',
         Gtk.Frame {
             Gtk.TextView {
-              buffer = buffer
+              buffer = buffer,
+              editable = false,
             }
         },
         Gtk.Frame {
@@ -24,13 +54,35 @@ local window = Gtk.Window {
             orientation = 'HORIZONTAL',
             border_width = 5,
             spacing = 80,
-            Gtk.Button { use_stock = true, label = Gtk.STOCK_OK },
-            Gtk.Button { use_stock = true, label = Gtk.STOCK_CANCEL }
+            Gtk.Button {
+              id = "ok_button",
+              use_stock = true,
+              label = Gtk.STOCK_OK
+            },
+            Gtk.Button {
+              id = "cancel_button",
+              use_stock = true,
+              label = Gtk.STOCK_CANCEL
+            }
           }
         },
       },
       on_destroy = Gtk.main_quit
 }
 
+local home = os.getenv("HOME")
+
+function window.child.ok_button:on_clicked()
+  io.popen("fcitx5-migrator")
+  local f = io.open(home .. "/.config/fcitx/.migration-complete", "w")
+  io.close(f)
+  window:destroy()
+end
+
+function window.child.cancel_button:on_clicked()
+  local f = io.open(home .. "/.config/fcitx5/.timestamp", "w")
+  io.close(f)
+  window:destroy()
+end
 window:show_all()
 Gtk.main()
